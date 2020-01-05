61aa6f71c63597479df19f5d00a09d29df55cd62
sagivt1 <sagiv.talekr@gmail.com>
2020-01-05 15:32:37 +0200

    update person

--- Person.py
+++ Person.py
@@ -1,24 +1,23 @@
-
 import database
 
 
 class Person:
 
-    def __init__(self, f_name = None, l_name = None, ID = None, user = None, password = None):
+    def __init__(self, f_name=None, l_name=None, i_d=None, user=None, password=None):
 
-        self.data = database.DataBase(self.first_name,self.last_name,self.id,self.user_name,self.password)
+        self.data = database.DataBase(self.first_name, self.last_name, self.id, self.user_name, self.password)
         self.first_name = f_name
         self.last_name = l_name
-        self.id = ID
+        self.i_d = i_d
         self.user_name = user
         self.password = password
 
     def register(self):
         self.first_name = input("First Name:")
-        self.last_name=input("Last Name:")
-        self.id = input("enter your ID (9 digits):")
+        self.last_name = input("Last Name:")
+        self.i_d = input("enter your ID (9 digits):")
         while len(self.id) != 9:
-            self.id = input("invalid number! please enter your id:")
+            self.i_d = input("invalid number! please enter your id:")
         self.user_name = input("Please enter user name:")
         self.password = input("enter a password:")
 
@@ -29,4 +28,3 @@ class Person:
             print("WRONG USERNAME OR PASSWORD! PLEASE TRY AGAIN")
             self.user_name = input("Please enter user name:")
             self.password = input("enter a password:")
-
